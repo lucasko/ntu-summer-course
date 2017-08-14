6f91f05945983c62ad1463b553ec7e6ee46f3b54
Lucas Ko <lucasko.tw@gmail.com>
2017-08-14 11:55:41 +0800

    download

---
+++ download.sh
@@ -0,0 +1,7 @@
+wget https://download.elastic.co/kibana/kibana/kibana-4.1.3-linux-x64.tar.gz
+
+wget https://download.elastic.co/logstash/logstash/logstash-2.3.4.tar.gz
+
+wget wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.0/elasticsearch-2.3.0.tar.gz
+
+
